bb691e254b7711b38cfc6dcb1aefa8e82b8accdf
moksha1818 <moksha.s1803@gmail.com>
2025-08-03 22:56:01 +0530

    Task 3

---
+++ Task 3/test.js
@@ -0,0 +1,35 @@
+ // Assignment Operators
+
+    let a = 20;
+    a += 5;
+    console.log(a, "Addition Assignment Operator")
+
+    let b = 15;
+    b -= 2;
+    console.log(b, "Subtraction  Assignment Operator")
+
+    let c = 20;
+    a *= 5;
+    console.log(a, "Multiplication Assignment Operator")
+
+    let d = 50;
+    d /= 2;
+    console.log(d, "Division Assigment Operator")
+
+    let e = 25;
+    e %= 5;
+    console.log(e)
+
+    let f = 2 ;
+    f **= 5;
+    console.log(f)
+
+
+
+      
+
+       
+        
+
+
+   
